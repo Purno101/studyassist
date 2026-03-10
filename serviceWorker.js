9a2f07563efa99240f71a1aa1a0733395c2481a0
Kazuha <150519197+Purno101@users.noreply.github.com>
2026-03-10 07:33:57 +0600

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -15,62 +15,56 @@ const FILES_TO_CACHE = [
   "./sounds/acceptBoss.mp3"
 ]
 
+// INSTALL
 self.addEventListener("install", event => {
-  console.log("[ServiceWorker] Install")
-  
+
+  self.skipWaiting()
+
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log("[ServiceWorker] Caching app files")
-        return cache.addAll(FILES_TO_CACHE)
-      })
-      .then(() => {
-        console.log("[ServiceWorker] Skip waiting on install")
-        return self.skipWaiting()
-      })
+      .then(cache => cache.addAll(FILES_TO_CACHE))
   )
+
 })
 
-
+// ACTIVATE
 self.addEventListener("activate", event => {
-  console.log("[ServiceWorker] Activate")
-  
+
   event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => {
-        if (key !== CACHE_NAME) {
-          console.log("[ServiceWorker] Removing old cache", key)
-          return caches.delete(key)
-        }
-      }))
-    })
-  )
-  
-  
-  return self.clients.claim()
-})
-
-
-self.addEventListener("fetch", event => {
-
-  if (event.request.url.includes('localhost') || event.request.mode === 'navigate') {
-   
-    event.respondWith(
-      caches.match('index.html').then(response => {
-        return response || fetch(event.request)
-      })
-    )
-  } else {
-   
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request).catch(() => {
-          
-          if (event.request.url.match(/\.(jpg|jpeg|png|gif|mp3|wav)$/)) {
-            return new Response('', { status: 404 })
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key)
           }
         })
+      )
+    }).then(() => self.clients.claim())
+  )
+
+})
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request).then(cached => {
+
+      if (cached) {
+        return cached
+      }
+
+      return fetch(event.request).catch(() => {
+
+        if (event.request.mode === "navigate") {
+          return caches.match("./index.html")
+        }
+
       })
-    )
-  }
+
+    })
+
+  )
+
 })
